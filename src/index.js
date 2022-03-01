3730fe21d6cd271112826a04eae3e28781cfe009
Grecia Villacorta <ggarciavillacort@tripadvisor.com>
2022-03-01 09:55:50 -0500

    implement register form with a single state value

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Login from './Login';
+import Register from './Register';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Login />
+    <Register />
   </React.StrictMode>,
   document.getElementById('root')
 );
